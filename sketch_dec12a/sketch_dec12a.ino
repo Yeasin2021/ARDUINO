8a154bc595f7103b96f318958a69e30e367eb2ee
unknown <yeasin18801@gmail.com>
2022-12-24 00:56:42 +0600

    Multiple LED Blinkings

--- sketch_dec12a/sketch_dec12a.ino
+++ sketch_dec12a/sketch_dec12a.ino
@@ -1,15 +1,27 @@
 
-    int led = 13;
+    int led_1 = 13;
+    int led_2 = 12;
+    int led_3 = 11;
     
     void setup() {
       // put your setup code here, to run once:
-      pinMode(led,OUTPUT);
+      pinMode(led_1,OUTPUT);
+      pinMode(led_2,OUTPUT);
+      pinMode(led_3,OUTPUT);
     }
     
     void loop() {
       // put your main code here, to run repeatedly:
-      digitalWrite(led,HIGH);
-      delay(500);
-      digitalWrite(led,LOW);
-      delay(500);
+      digitalWrite(led_1,HIGH);
+      delay(100);
+      digitalWrite(led_1,LOW);
+      delay(100);
+      digitalWrite(led_2,HIGH);
+      delay(100);
+      digitalWrite(led_2,LOW);
+      delay(100);
+      digitalWrite(led_3,HIGH);
+      delay(100);
+      digitalWrite(led_2,LOW);
+      delay(100);
     }

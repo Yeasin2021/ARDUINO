4f753cf4c70ad6459b10754924c6f4fb7c9b25d5
unknown <yeasin18801@gmail.com>
2023-01-02 23:30:35 +0600

    blinking light of temperature comments

--- temp_con/temp_con.ino
+++ temp_con/temp_con.ino
@@ -1,22 +1,22 @@
 
-#include "DHT.h"
-#define DHTTYPE DHT11
-#define DHTPIN D5
+#include "DHT.h"              // inckude libery
+#define DHTTYPE DHT11         // declare dht11 
+#define DHTPIN D5             // declare pin number 1 for data
 
-DHT dht(5,DHTTYPE);
+DHT dht(5,DHTTYPE);           // call dht object 
 
 void setup(){
   dht.begin();
-  Serial.begin(115200);
-  pinMode(DHTPIN, OUTPUT);
+  Serial.begin(115200);       // boudrate declare
+  pinMode(DHTPIN, OUTPUT);    
   Serial.println('Humidity and Temperature\n\n');
   delay(1000);
 }
 
 void loop()
 {
-  float h = dht.readHumidity();
-  float t = dht.readTemperature();
+  float h = dht.readHumidity();         // read humidity
+  float t = dht.readTemperature();      // read temperature
   Serial.print("Current Humidity = ");
   Serial.print(h);
   Serial.print("% ");
@@ -24,7 +24,7 @@ void loop()
   Serial.print(t);
   Serial.println(" C ");
   delay(1000);
-  if(t <= 30){
+  if(t <= 30){                        // condition check, when temperature is bellow 30 then the red light is blinking 
       digitalWrite(DHTPIN, HIGH);  
       delay(1000);                     
       digitalWrite(DHTPIN, LOW);  
